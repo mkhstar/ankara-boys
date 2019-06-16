5141856900e74a526a84b19b9a1a1d177479f012
Kusi Musah Hussein <kusimusah@gmail.com>
2019-06-16 14:57:12 +0300

    first version

---
+++ src/main.js
@@ -0,0 +1,58 @@
+const users = [{
+    userName: "Musah Kusi Hussein mkhstar"
+  },
+  {
+    userName: "Mohammed SharifDeen"
+  },
+  {
+    userName: "Vandana Saifullah"
+  },
+  {
+    userName: "Yusif Mustapha"
+  }
+]
+
+const startDate = 1560546000000; // 15th June, 2019
+const todaysDate = new Date().getTime();
+const weekInterval = 604800000;
+const weeksToday = (todaysDate - startDate) / weekInterval;
+const indexCalc = Math.floor(weeksToday) % 4;
+const dutyPlaces = ["BATHROOM", "FRIDGE AND FLOOR (KITCHEN AND ITS BALCONY)", "COOKING AREA AND WOODWORK", "CORRIDOR AND TOILET"];
+
+
+const tbody = document.querySelector(".duty-content tbody");
+const dayDate = document.querySelector(".day-and-date");
+const searchInput = document.querySelector("#searchDuty");
+
+if (dayDate) {
+  dayDate.innerHTML = new Date().toDateString() + ", " + new Date().toLocaleTimeString();
+}
+if (tbody) {
+  tbody.innerHTML = "";
+  users.forEach((user, i) => {
+    tbody.innerHTML += `
+    <tr data-name="${user.userName}">
+    <td>${user.userName}</td>
+    <td>${dutyPlaces[indexCalc + i]}</td>
+    </tr>
+    `;
+  })
+
+}
+
+if (searchInput) {
+  searchInput.addEventListener("keyup", e => {
+    const {
+      value
+    } = e.target;
+    const rows = tbody.querySelectorAll("tr");
+    rows.forEach(row => {
+      const userName = row.getAttribute('data-name');
+      if (value.length > 0) {
+        if (userName.toLowerCase().indexOf(value.toLowerCase()) < 0) {
+          row.className = "hide";
+        } else row.className = "";
+      } else row.className = "";
+    })
+  })
+}
