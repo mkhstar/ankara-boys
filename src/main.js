ee34a8495c0e12551af6f4b6554518a8b4b030a4
Kusi Musah Hussein <kusimusah@gmail.com>
2020-07-14 23:17:53 +0300

    custom date

--- src/main.js
+++ src/main.js
@@ -1,33 +1,31 @@
 const users = [
   {
-    userName: "Musah Kusi Hussein mkhstar"
+    userName: "Musah Kusi Hussein mkhstar",
   },
   {
-    userName: "Mohammed SharifDeen"
+    userName: "Mohammed SharifDeen",
   },
   {
-    userName: "Vandana Saifullah"
+    userName: "Vandana Saifullah",
   },
   {
-    userName: "Yusif Mustapha"
+    userName: "Yusif Mustapha",
   },
   {
-    userName: "Umar Sharif Asumah Paper"
-  }
+    userName: "Umar Sharif Asumah Paper",
+  },
 ];
 
-const startDate = new Date('11 July 2020').getTime();
+const startDate = new Date("11 July 2020").getTime();
 const todaysDate = new Date().getTime();
 const weekInterval = 604800000;
-const weeksToday = (todaysDate - startDate) / weekInterval;
-const indexCalc = Math.floor(weeksToday) % users.length;
 
 const dutyPlaces = [
   "BATHROOM",
   "FRIDGE AND FLOOR (KITCHEN AND ITS BALCONY)",
   "COOKING AREA AND WOODWORK",
   "TOILET",
-  "CORRIDOR"
+  "CORRIDOR",
 ];
 
 const tbody = document.querySelector(".duty-content tbody");
@@ -39,26 +37,14 @@ if (dayDate) {
     new Date().toDateString() + ", " + new Date().toLocaleTimeString();
 }
 if (tbody) {
-  tbody.innerHTML = "";
-  users.forEach((user, i) => {
-    const dutyIndex =
-      indexCalc + i >= users.length
-        ? Math.abs(users.length - (indexCalc + i))
-        : indexCalc + i;
-    tbody.innerHTML += `
-    <tr data-name="${user.userName}">
-    <td>${user.userName}</td>
-    <td>${dutyPlaces[dutyIndex]}</td>
-    </tr>
-    `;
-  });
+  tbody.innerHTML = getTableHtml(startDate, todaysDate);
 }
 
 if (searchInput) {
-  searchInput.addEventListener("keyup", e => {
+  searchInput.addEventListener("keyup", (e) => {
     const { value } = e.target;
     const rows = tbody.querySelectorAll("tr");
-    rows.forEach(row => {
+    rows.forEach((row) => {
       const userName = row.getAttribute("data-name");
       if (
         value.length > 0 &&
@@ -68,4 +54,54 @@ if (searchInput) {
       } else row.className = "";
     });
   });
+}
+
+const showCustomBtn = document.getElementById("show-custom-btn");
+const customDate = document.getElementById("custom-date");
+const customDateModal = document.getElementById('custom-date-modal');
+const customCloseDateModal = document.getElementById('close-modal-custom-date');
+
+if (showCustomBtn) showCustomBtn.addEventListener("click", () => customDateModal.classList.add('show'));
+
+if (customCloseDateModal) customCloseDateModal.addEventListener('click', () => customDateModal.classList.remove('show'))
+
+if (customDate) {
+  const date = new Date();
+
+  customDate.setAttribute("max", date.toISOString().slice(0, 10));
+  customDate.addEventListener("change", (e) => {
+    const { value: chosenDate } = e.target;
+    document.querySelector(".day-and-date-modal").innerHTML =
+      new Date(chosenDate).toDateString() +
+      ", " +
+      new Date(chosenDate).toLocaleTimeString();
+
+    const customTbody = document.querySelector(".modal-table-card tbody");
+
+    customTbody.innerHTML = getTableHtml(
+      startDate,
+      new Date(chosenDate).getTime()
+    );
+  });
+}
+
+function getTableHtml(startDate, endDate) {
+  const weeksToday = (endDate - startDate) / weekInterval;
+  const indexCalc = Math.floor(weeksToday) % users.length;
+
+  let html = "";
+  users.forEach((user, i) => {
+    const dutyIndex =
+      indexCalc + i >= users.length
+        ? Math.abs(users.length - (indexCalc + i))
+        : indexCalc + i;
+
+    html += `
+    <tr data-name="${user.userName}">
+    <td>${user.userName}</td>
+    <td>${dutyPlaces[dutyIndex]}</td>
+    </tr>
+    `;
+  });
+  return html;
 }
